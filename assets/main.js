fe2e4339b1f3a11041e96f5fff00271a27a97224
r-e-d-ant <mugishathierry65@gmail.com>
2021-11-05 18:51:15 +0200

    commented the javaScript code

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,16 @@
 
+// Navigation bar toggling
+
 const navbar = document.querySelector('.navbar')
 const menuOpen = document.querySelector('.bx-menu')
 const menuClose = document.querySelector('.bx-x')
 
+// Open Menu
 menuOpen.addEventListener('click', () => {
+    /*
+    check if showMenu class in not in navbar
+    If not show it
+    */
     if(!navbar.classList.contains('showMenu')) {
         navbar.classList.toggle('showMenu')
         menuOpen.classList.toggle('hide')
@@ -11,7 +18,12 @@ menuOpen.addEventListener('click', () => {
     }
 })
 
+// Close menu
 menuClose.addEventListener('click', () => {
+    /*
+    check if showMenu class in in navbar
+    If true remove it
+    */
     if(navbar.classList.contains('showMenu')) {
         navbar.classList.remove('showMenu')
         menuOpen.classList.remove('hide')
